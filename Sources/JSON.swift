ec150da5c94047695039946c5374dcd902204a3d
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-26 20:48:56 +0100

    consistentify

--- Sources/JSON.swift
+++ Sources/JSON.swift
@@ -70,13 +70,20 @@ public func ==(lhs: JSON.Number, rhs: JSON.Number) -> Bool {
 extension JSON: Equatable { }
 public func ==(lhs: JSON, rhs: JSON) -> Bool {
     switch (lhs, rhs) {
-    case (.null, .null): return true
-    case (.boolean(let l), .boolean(let r)): return l == r
-    case (.string(let l), .string(let r)): return l == r
-    case (.number(let l), .number(let r)): return l == r
-    case (.array(let l), .array(let r)): return l == r
-    case (.object(let l), .object(let r)): return l == r
-    default: return false
+    case (.null, .null): 
+        return true
+    case (.boolean(let l), .boolean(let r)): 
+        return l == r
+    case (.string(let l), .string(let r)): 
+        return l == r
+    case (.number(let l), .number(let r)): 
+        return l == r
+    case (.array(let l), .array(let r)): 
+        return l == r
+    case (.object(let l), .object(let r)): 
+        return l == r
+    default: 
+        return false
     }
 }
 
